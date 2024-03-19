96b82f66c4f5b75ecf819f307cc29b9b9e67614a
Paulo Henrique <paulohenriquemr555@gmail.com>
2024-03-19 16:21:05 -0300

    Exercicio 5

---
+++ ExercicioCinco/ExercicioCinco/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quinto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string normal = "Target";
+            string inversa = StringInversa(normal);
+            Console.WriteLine(inversa);
+        }
+
+        static string StringInversa (string str)
+        {
+            //Pega todas as letras presentes na string e coloca em uma array de chars
+            char[] letras = str.ToCharArray();
+
+            //Usa do metodo Reverse presente na Array para inverter a ordem das letras no array
+            Array.Reverse(letras);
+
+            //retorna a string
+            return new string(letras);
+        }
+    }
+}
