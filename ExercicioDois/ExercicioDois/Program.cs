9fdd583c7ec542d8c47bb8451e6ca482dff4904d
Paulo Henrique <paulohenriquemr555@gmail.com>
2024-03-19 15:41:03 -0300

    Exercicio 2

--- ExercicioDois/ExercicioDois/Program.cs
+++ ExercicioDois/ExercicioDois/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Segundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacci = new List<int>{ 0, 1, 1, 2 };
+            int num = 13;
+
+            while(fibonacci.Count < num) 
+            {
+                var anterior1 = fibonacci[fibonacci.Count - 1];
+                var anterior2 = fibonacci[fibonacci.Count - 2];
+
+                int prox = anterior1 + anterior2;
+
+                fibonacci.Add(prox);
+                Console.WriteLine(prox);
+
+            }
+
+            if (fibonacci.Contains(num))
+            {
+                Console.WriteLine($"A sequência contém {num}");
+            }
+            else
+            {
+                Console.WriteLine($"A sequência não contém {num}");
+            }
+
+            
+
+            
+            
+        }
+    }
+}
