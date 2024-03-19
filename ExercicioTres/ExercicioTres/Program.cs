31df5553e73027ff2a395ecfdbe1d2f38ba1e020
Paulo Henrique <paulohenriquemr555@gmail.com>
2024-03-19 15:54:01 -0300

    Exercicio 3

---
+++ ExercicioTres/ExercicioTres/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Terceiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A) No exercício A o próximo número é 9 -> 1, 3, 5, 7, (9)
+
+            //B) No exercício B o próximo número é 128 -> 2, 4, 8, 16, 32, 64, (128)
+            
+            //C) No exercício C o próximo número é 49 -> 0, 1, 4, 9, 16, 25, 36, (49)
+
+            //D) No exercício D o próximo número é 100 -> 4, 16, 36, 64, (100)
+
+            //E) No exercício E o próximo número é 13 -> 0, 1, 1, 2, 3, 5, 8, (13)
+
+            //F) No exercício F o próximo número é 200 -> Todos começam com D - 2, 10, 12, 16, 17, 18, 19, (200)
+        }
+    }
+}
